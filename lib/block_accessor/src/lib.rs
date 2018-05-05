fe52fb72235078eb4014023ed25874c900d506d9
Stephen Molyneaux <shmolyne@uwaterloo.ca>
2018-05-04 22:46:39 -0400

    Add dependent block_accessor lib

---
+++ lib/block_accessor/src/lib.rs
@@ -0,0 +1,10 @@
+pub enum BlockAccessError {
+    BlockOutOfRange,
+    MiscError
+}
+
+pub trait BlockAccessor {
+    fn block_size(&self) -> u64;
+    fn read_block(&mut self, block_num: u64, block: &mut [u8]);
+    fn write_block(&mut self, block_num: u64, block: &[u8]) -> Result<(), BlockAccessError>;
+}
---
+++ lib/block_accessor/target/debug/libblock_accessor.d
@@ -0,0 +1,1 @@
+/home/stephen/projects/messd/lib/block_accessor/target/debug/libblock_accessor.rlib: /home/stephen/projects/messd/lib/block_accessor/src/lib.rs
